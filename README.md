b436b56662af939c6f8baae6afd43f5e127a074e
TuxInvader <tuxinvader@github.com>
2023-11-28 09:05:06 +0000

    introduce some randomness

--- README.md
+++ README.md
@@ -1,28 +1,36 @@
 # Wrk container for load tests
 
-This container will run a number of `wrk` processes. The exact number is determined by the number of cores in the system
-multiplied by the `CPU_SCALING` factor.
+This container will run a number of `wrk` processes. The exact number is determined by the number of
+cores in the system multiplied by the `CPU_SCALING` factor.
 
-Each process will run `THREADS` threads and create `CONCURRENT` connections to the target, and run for `DURATION` time.
+Each process will run `THREADS` threads and create `CONCURRENT` connections to the target,
+and run for `DURATION` (minutes) time.
 
-The targets will be selected randomly from the `IPS`, which is a space seperated list of IP addresses. The host header
-used when connecting will be determined by the `HOST` setting, and the URI Path by the `URI`.
+The targets will be selected randomly from the `IPS`, which is a space seperated list of IP addresses.
+The Host header used when connecting will be determined by the `HOST` setting, and the URI Path by the `URI`.
 
-The protocol used will be determined by the `PROTO` setting.
+The protocol used will be determined by the `PROTO` (http|https) setting, and the port will be
+the standard port unless you provide a `PORT`.
 
 Defaults
 ```
-ENV CONCURRENT="500"
+ENV CONCURRENT="50"
 ENV THREADS="1"
 ENV CPU_SCALING="1"
-ENV DURATION="1h"
+ENV DURATION="1"
 ENV HOST="www.example.com"
 ENV IPS="127.0.0.1 127.0.1.1"
 ENV URI="/"
 ENV PROTO="http"
+ENV PAUSE="10"
+ENV RANDOMIZE="false"
 ```
 
 You may also provide a `PORT` parameter if you are running the `PROTO` (http|https) on a non-standard port.
+
+## RANDOMIZE
+If you set `RANDOMIZE=true` then all other values are considered maximums and each wrk process will launch
+with a value between 1 and the current setting.
 
 Download from docker hub: [tuxinvader/wrkit](https://hub.docker.com/r/tuxinvader/wrkit)
 
--- run.sh
+++ run.sh
@@ -3,11 +3,13 @@
 [ -z "${CONCURRENT+x}" ] && CONCURRENT="100"
 [ -z "${THREADS+x}" ] && THREADS="1"
 [ -z "${CPU_SCALING+x}" ] && CPU_SCALING="1"
-[ -z "${DURATION+x}" ] && DURATION="30m"
+[ -z "${DURATION+x}" ] && DURATION="30"
 [ -z "${HOST+x}" ] && HOST="www.example.com"
 [ -z "${IPS+x}" ] && IPS="127.0.0.1 127.0.1.1"
 [ -z "${PROTO+x}" ] && PROTO="http"
 [ -z "${URI+x}" ] && URI="/foo"
+[ -z "${RANDOMIZE+x}" ] && RANDOMIZE="false"
+[ -z "${PAUSE+x}" ] && PAUSE="1"
 
 if [ -z "${PORT+x}" ]
 then
@@ -30,6 +32,25 @@ _term() {
   running=false
 }
 
+launch() {
+  cpu=$1
+  scaling=$2
+  concurrent=$3
+  threads=$4
+  duration=$5
+  pause=$6
+
+  for procs in $(seq 0 $(( $scaling -1 )) )
+  do
+    target="${IP_ARRAY[$(( $RANDOM % ${#IP_ARRAY[*]} ))]}"
+    date +"%F %T: Launching: taskset -c $cpu wrk -c ${concurrent} -t ${threads} -d ${duration}m -H \"Host: ${HOST}\" \"${PROTO}://${target}:${PORT}${URI}\""
+    taskset -c "$cpu" wrk -c "${concurrent}" -t "${threads}" -d "${duration}m" -H "Host: ${HOST}" "${PROTO}://${target}:${PORT}${URI}" &
+    wrks="${wrks} $!"
+  done
+  date +"%F %T: Pausing: $pause seconds"
+  sleep $pause
+}
+
 trap _term SIGTERM
 
 IP_ARRAY=( $IPS )
@@ -40,19 +61,24 @@ date +"%F %T: Target IPS: ${IPS}"
 
 for cpu in $(seq 0 $((  $CPUS -1  )) )
 do
-  for procs in $(seq 0 $(( $CPU_SCALING -1 )) )
-  do
-    target="${IP_ARRAY[$(( $RANDOM % ${#IP_ARRAY[*]} ))]}"
-    date +"%F %T: Launching: taskset -c $cpu wrk -c ${CONCURRENT} -t ${THREADS} -d ${DURATION} -H \"HOST: ${HOST}\" \"${PROTO}://${target}:${PORT}${URI}\""
-    taskset -c $cpu wrk -c ${CONCURRENT} -t ${THREADS} -d ${DURATION} -H "HOST: ${HOST}" "${PROTO}://${target}:${PORT}${URI}" &
-    wrks="${wrks} $!"
-  done
-  sleep 1
+  if [ "$RANDOMIZE" == "true" ]
+  then
+    scaling=$(( $RANDOM % $CPU_SCALING + 1 ))
+    threads=$(( $RANDOM % $THREADS + 1 ))
+    concurrent=$(( $RANDOM % $CONCURRENT + $threads ))
+    duration=$(( $RANDOM % $DURATION + 1 ))
+    pause=$(( $RANDOM % $PAUSE + 1 ))
+    launch "$cpu" "$scaling" "$concurrent" "$threads" "$duration" "$pause"
+  else
+    launch "$cpu" "$CPU_SCALING" "$CONCURRENT" "$THREADS" "$DURATION" "$PAUSE"
+  fi
 done
 
+sleeps=0
 while [ "$running" == "true" ];
 do
-  date +"%F %T: Waiting for wrk to be done"
+  sleeps=$(( $sleeps + 1 ))
+  [ "$(( $sleeps % 6 ))" -eq "0" ] && date +"%F %T: Waiting for wrk to be done"
   sleep 5
   procs=$(ps -ef | grep wrk | grep -v grep)
   if [ -z "${procs}" ]
